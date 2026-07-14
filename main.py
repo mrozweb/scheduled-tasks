e31bf43d26069111118df8f5b5a96e65321bb766
mrozweb <deboerbeata@gmail.com>
2026-07-14 14:49:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import random
 import smtplib
 import datetime as dt
 
-MY_EMAIL = "deboerbeata@gmail.com"
-PASSWORD = "ibfdodlwkwqhoyrx"
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 now = dt.datetime.now()
 weekday = now.weekday()
 
-if weekday == 6:
+if weekday == 0:
     with open("quotes.txt") as file:
         quotes = [quote.strip() for quote in file.readlines()]
         random_quote = random.choice(quotes)
@@ -20,6 +20,6 @@ if weekday == 6:
         connection.login(MY_EMAIL, PASSWORD)
         connection.sendmail(
             from_addr=MY_EMAIL ,
-            to_addrs="doradada84@gmail.com",
+            to_addrs="deboerbeata3@gmail.com",
             msg=f"Subject:Monday Motivation\n\n{random_quote}"
         )
